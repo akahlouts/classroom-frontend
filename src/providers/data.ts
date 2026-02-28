7275f798b42384ee0acd7a577b897dff81c510cb
AbdElRahman KahlOut <akahlouts@gmail.com>
2026-03-01 00:32:59 +0200

    implement class details page

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -2,7 +2,7 @@ import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 import { HttpError } from "@refinedev/core";
 
 import { BACKEND_BASE_URL } from "@/constants";
-import { CreateResponse, ListResponse } from "@/types";
+import { CreateResponse, GetOneResponse, ListResponse } from "@/types";
 
 if (!BACKEND_BASE_URL)
   throw new Error(
@@ -80,6 +80,16 @@ const options: CreateDataProviderOptions = {
 
     mapResponse: async (response) => {
       const json: CreateResponse = await response.json();
+
+      return json.data ?? [];
+    },
+  },
+
+  getOne: {
+    getEndpoint: ({ resource, id }) => `${resource}/${id}`,
+
+    mapResponse: async (response) => {
+      const json: GetOneResponse = await response.json();
 
       return json.data ?? [];
     },
